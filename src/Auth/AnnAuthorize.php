ec97e35f1e2328e18f7a9891d183979a5e698998
Michael Iseli <michael@crazymonkeys.de>
2022-10-12 22:25:04 +0200

    Upgrade to CakePHP 4.x (#1)

--- src/Auth/AnnAuthorize.php
+++ src/Auth/AnnAuthorize.php
@@ -18,12 +18,12 @@ class AnnAuthorize extends BaseAuthorize {
      * @param ServerRequest $request
      *         Describes the request to authorize.
      */
-    public function authorize($user, ServerRequest $request) {
+    public function authorize($user, ServerRequest $request): bool {
         $controller = $this->_registry->getController();
-        $action = $request->param('action');
-        $pass = $request->param('pass');
+        $action = $request->getParam('action');
+        $pass = $request->getParam('pass');
         Log::debug(sprintf('Trying to authorize user %s for request %s/%s and parameters %s.',
-                $user['username'], $controller->name, $action, json_encode($pass)));
+                $user['username'], $controller->getName(), $action, json_encode($pass)));
         $annAuthorization = AnnAuthorization::getInstance();
         $authorized = $annAuthorization->authorizeRequest($user['id'], $controller, $action, $pass, $request);
         Log::debug(sprintf('Authorization %s', $authorized ? 'was successful.': 'failed.'));
